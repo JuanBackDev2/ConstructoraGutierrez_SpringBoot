328e0fd192a9d630b697c5355552d60cc88a62bb
JuanBackDev2 <juantenis13@gmail.com>
2023-04-27 22:53:19 -0500

    first commit

---
+++ src/test/java/com/constructora/gutierrez/ConstructoraGutierrezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructora.gutierrez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructoraGutierrezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
